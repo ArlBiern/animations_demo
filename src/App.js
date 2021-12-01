47bf892236a6f3e8071bd24f2ba9a8a0f6215c7e
ArlBiern <arielbiernacki@gmail.com>
2021-12-01 12:59:36 +0100

    Add ConstructionAnimation with basic movement

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Here animations will be created</div>;
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import ConstructionAnimation from "./ConstructionAnimation";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here animations will be created</h1>
+      <ConstructionAnimation />
+    </div>
+  );
+}
+
+export default App;
